59a5a21cb2759ff83e257fb0521b3b0464a52488
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-10-03 02:52:23 -0300

    api endpoint changed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,13 +7,13 @@ const Home = () => {
 
   useEffect(() => {
     const getPokemons = async () => {
-      const result = await axios('https://pokeapi.co/api/v2/pokemon?limit=151');
-      const data = result.data.results;
-      const pokemonsParsed = data.map(pokemon => {
+      const result = await axios('https://pokeapi.co/api/v2/generation/1');
+      const data = result.data;
+      const pokemonsParsed = data.pokemon_species.map(pokemon => {
         let splitUrl = pokemon.url.split('/');
         let id = splitUrl[splitUrl.length - 2];
         return { id: id, name: pokemon.name }
-      });
+      }).sort((a, b) => a.id - b.id);
       setPokemons(pokemonsParsed);
     };
     getPokemons();
@@ -35,7 +35,7 @@ const Home = () => {
             {pokemons.map(pokemon => (
               <tr key={pokemon.id}>
                 <td>{pokemon.id}</td>
-                <td>{pokemon.name}</td>
+                <td><a href={`/pokemon/${pokemon.id}`}>{pokemon.name}</a></td>
               </tr>
             ))}
           </tbody>
--- src/pages/information/Information.jsx
+++ src/pages/information/Information.jsx
@@ -1,9 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import Header from '../../components/Header';
+
 const Information = () => {
+  let { id } = useParams();
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const result = await axios(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = result.data;
+      console.log(data);
+      setPokemon(data);
+    };
+    getPokemon();
+  }, [id]);
 
   return (
-    <div>
-      <h1>Information</h1>
-    </div>
+    <>
+      <Header />
+      <main className="container">
+        <h1>Information</h1>
+        <div>
+          <h2>{pokemon.name}</h2>
+          <div className="img-container">
+            <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
