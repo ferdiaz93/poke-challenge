7bb5a006e255bc598ac84f990fcec0e799778950
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-10-03 01:36:50 -0300

    init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/images/anoctua-logo.png'
+import burgerSvg from '../assets/images/burger-menu.svg'
+
+const Header = () => {
+  return (
+    <header className="header wrapper">
+      <h1>Poke-Challenge</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import Header from '../../components/Header'
+
+const Home = () => {
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <>
+      <Header />
+    <main>
+      <h1>Home</h1>
+    </main>
+    </>
+  )
+}
+
+export default Home
